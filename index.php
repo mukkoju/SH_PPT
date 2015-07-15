a556a388b70cbd1e5c42f878ea390aa1d0326b91
satish <sm@viveninfomedia.com>
2015-07-15 12:39:35 +0530

    header added

---
+++ index.php
@@ -0,0 +1,44 @@
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Saddahaq presentation</title>
+        <meta name="description" content=""/>
+        <meta name="keywords" content="" />
+        <meta name="news_keywords" content="">
+        <link rel="icon" type="image/png" href="https://saddahaq.blob.core.windows.net/multimedia/favicon.ico">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+        <link href="/public/css/layout.css?v=1" media="screen" rel="stylesheet" type="text/css" />
+        <link href="/public/global/subsidy/subsidy.css?v=6" media="screen" rel="stylesheet" type="text/css" />
+        <link href="/public/css/saddahaq.css?v=1" media="screen" rel="stylesheet" type="text/css" />
+        <link href='//fonts.googleapis.com/css?family=Gentium+Basic:400,400italic|Noto+Sans:400,500,700,400italic,700italic' rel='stylesheet' type='text/css'>
+        <script type="text/javascript" src="/public/js/jquery.min.js?v=1"></script>
+        <script type="text/javascript" src="/public/js/smoothscroll.js?v=1"></script>
+        <script type="text/javascript" src="/public/js/countTo.js?v=1"></script>
+        <script type="text/javascript" src="/public/js/jquery.appear.js?v=1"></script>
+        <script type="text/javascript" src="/public/js/custom.js?v=1"></script>
+    </head>
+    <body>
+        <div id="navigation-bar" class="box">
+            <div class="navbar box">
+                <p class="lg-hm pull-left">
+                        <a href="/" class="transition in" id="logo">
+                            <img src="https://testing.saddahaq.com/public/images/lp_logo.png">
+                        </a>
+                    </p>
+                <div id="main-mnu">
+                    <ul class="clearfix">
+                        <li class="pull-left"><a href="#">PROBLEM</a></li>
+                        <li class="pull-left"><a href="#">OPPORTUNITY</a></li>
+                        <li class="pull-left"><a href="#">SOLUTION</a></li>
+                        <li class="pull-left"><a href="#">MARKET SIZE</a></li>
+                        <li class="pull-left"><a href="#">PRODUCT</a></li>
+                        <li class="pull-left"><a href="#">REVENUE MODEL</a></li>
+                        <li class="pull-left"><a href="#">COMPETITORS</a></li>
+                        <li class="pull-left"><a href="#">STATISTICS</a></li>
+                        <li class="pull-left"><a href="#">TEAM</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>    
