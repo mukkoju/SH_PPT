599e97f69ae70722e3476f67219dd805efc89b7f
satish <sm@viveninfomedia.com>
2015-07-16 11:34:14 +0530

    public/css/layout.css

--- index.php
+++ index.php
@@ -8,9 +8,10 @@
         <link rel="icon" type="image/png" href="https://saddahaq.blob.core.windows.net/multimedia/favicon.ico">
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
         <link href="/public/css/layout.css?v=1" media="screen" rel="stylesheet" type="text/css" />
-        <link href="/public/global/subsidy/subsidy.css?v=6" media="screen" rel="stylesheet" type="text/css" />
+        <link href="/public/css/view.css?v=6" media="screen" rel="stylesheet" type="text/css" />
         <link href="/public/css/saddahaq.css?v=1" media="screen" rel="stylesheet" type="text/css" />
         <link href='//fonts.googleapis.com/css?family=Gentium+Basic:400,400italic|Noto+Sans:400,500,700,400italic,700italic' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Roboto+Slab:400,700' rel='stylesheet' type='text/css'>
         <script type="text/javascript" src="/public/js/jquery.min.js?v=1"></script>
         <script type="text/javascript" src="/public/js/smoothscroll.js?v=1"></script>
         <script type="text/javascript" src="/public/js/countTo.js?v=1"></script>
@@ -39,6 +40,59 @@
                     </ul>
                 </div>
             </div>
+        </div>
+        <div id="intro" class="intro-block container-fluid">
+           <div class="slogan cntnt">WE ARE BUILDING REVOLUTIONARY PRODUCTS</div>
+        </div>
+        <div class="container-fluid" id="disconnect">
+            <section>
+                <hr class="line left" noshade size="1" style="width: 20%">
+                <span class="bbl left"></span>
+            <img src="https://testing.saddahaq.com/public/Uploads/devendarg/70818812-man.png">
+                <hr class="line right" noshade size="1" style="width: 20%">
+                <div class="bbl right"></div>
+                <div class="discnt-icn left"><img src="https://testing.saddahaq.com/public/Uploads/dontha/74252220-2418.png"></div>
+            <p class="cntnt">The human, financial, geographic & philosophical limitations of the media have kept the last mile disconnected</p>
+            <h1 class="m-hd">Disconnected Last Mile</h1>
+            <hr class="line vertcle" noshade size="1" style="width: 16%">
+                <span class="bbl"></span>
+                <div class="discnt-icn right"><img src="https://testing.saddahaq.com/public/Uploads/dontha/27403744-newspaper1.png"></div>
+            </section>
+        </div>
+        <div class="container-fluid intro-block" id="world-map">
+            <section>
+            <p class="cntnt">Turning consumers o f news into an active producer by providing them space to participate in dialogue around current affairs.</p>
+            <hr class="line vertcle frst" noshade size="1" style="width: 4%">
+                <span class="bbl"></span>
+            <p class="cntnt">Translating dialogue into action by connecting the dots between news,dialogue and third party services in right context at the right time</p>
+            <hr class="line vertcle scnd" noshade size="1" style="width: 4%">
+                <span class="bbl"></span>
+            <p class="cntnt">Taking news, dialogue and action into the remotest parts of the country,and eventually to the world</p>            </section>
+        </div>
+        <div class="container-fluid" id="opportunity">
+            <section>
+                <h2 class="cntnt">An <strong style="text-decoration: underline">Opportunity</strong> to learn and change the lives of millions of people! Connecting the last mile has great social and financial potential.</h2>
+                <p class="cntnt">- Hundreds of millions of unheard voices and millions of untold/unheard stories(of tech, medicine, culture, politics etc) and wisdom</p>
+                <p class="cntnt">- Hundreds of millions of new audience for information and dialogue</p>
+                <p class="cntnt">- Translate information and dialogue into action/impact on the ground</p>
+                <p class="cntnt">- Huge potential for unrealized digital revenues in the last mile economy</p>
+            </section> 
+        </div>
+        <div class="container-fluid" id="scl-jrnlsm">
+              
+              <ul class="graph">
+                  <hr style="width: 165px; top:-8px; position: absolute; left: -17px">
+                  <li style="height: 120px"><span>590</span></li>
+                  <li style="height: 110px"><span>438</span></li>
+                  <li style="height: 90px"><span>330</span></li>
+                  <li style="height: 70px"><span>264</span></li>
+                  <li style="height: 40px"><span>200</span></li>
+                  <hr style="width: 138px; top: 61px; left:  78px; -webkit-transform: rotate(90deg); position: absolute">
+                </ul>
+              
+            <h2 class="cntnt"><strong style="text-decoration: underline">Social journalism</strong> is the answer</h2>
+            <p class="cntnt">A platform that leverages the power of Internet and Mobile to connect people, stories and communication will be game changer</p>
+          </section>
         </div>
     </body>
 </html>    
